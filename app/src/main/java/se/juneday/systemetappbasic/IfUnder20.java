05bec7eacc84b4ec6fd32dc03819e8c629774fd6
VictorKkarlsson <victor.karlsson.97@gmail.com>
2019-05-12 22:34:40 +0200

    Under 20 knapp går nu vidare till en text som förklarar varför användaren inte kommer vidare i appen.

---
+++ app/src/main/java/se/juneday/systemetappbasic/IfUnder20.java
@@ -0,0 +1,13 @@
+package se.juneday.systemetappbasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IfUnder20 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_if_under20);
+    }
+}
--- app/src/main/java/se/juneday/systemetappbasic/MainActivity.java
+++ app/src/main/java/se/juneday/systemetappbasic/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
   private Button Over20;
+  private Button Under20;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +22,23 @@ public class MainActivity extends AppCompatActivity {
         openMainProductList();
       }
     });
-  }
+
+      Under20 = (Button) findViewById(R.id.Under20);
+      Under20.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          openIfUnder20();
+        }
+      });
+      }
+
 
   public void openMainProductList() {
     Intent intent = new Intent(this, MainProductList.class);
+    startActivity(intent);
+  }
+  public void openIfUnder20() {
+    Intent intent = new Intent(this, IfUnder20.class);
     startActivity(intent);
   }
 }
